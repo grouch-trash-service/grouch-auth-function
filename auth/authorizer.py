c8d67c02a6d1d75a8fce784ec720b2fc8ca7e98e
Matt Porter <matthew.js.porter@gmail.com>
2020-06-07 01:32:55 -0400

    added .pylintrc

--- auth/authorizer.py
+++ auth/authorizer.py
@@ -1,4 +1,17 @@
+"""
+authorizer module for invoking lambda function to do authentication
+"""
+
+
 def lambda_handler(event, context):
+    """
+    lambda function for authenticating users
+    :param event: lambda event that contains the authorization token
+    :param context: lambda context
+    :return: auth policy denoting whether the user is authenticated or not
+    """
+    print(event)
+    print(context)
     if event['authorizationToken'] == 'password':
         effect = 'Allow'
         principal = 'user'
